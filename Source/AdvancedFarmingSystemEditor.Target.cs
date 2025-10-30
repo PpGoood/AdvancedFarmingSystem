b279dddd14ff5a02d6cc0a6af09411e94c41ba2f
unknown <peiyu.li@yingxiong.com>
2025-10-30 17:16:32 +0800

    build：引擎升级5.6.1

--- Source/AdvancedFarmingSystemEditor.Target.cs
+++ Source/AdvancedFarmingSystemEditor.Target.cs
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
@@ -8,7 +6,9 @@ public class AdvancedFarmingSystemEditorTarget : TargetRules
 	public AdvancedFarmingSystemEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		// 升级 BuildSettings
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "AdvancedFarmingSystem" } );
 	}
